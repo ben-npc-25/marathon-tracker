9a8035747a5417018f16e6b891161983ce2b6272
ben-npc-25 <ben135797531@gmail.com>
2025-12-12 11:46:30 +0900

    update month banner

--- src/App.jsx
+++ src/App.jsx
@@ -1006,8 +1006,8 @@ export default function App() {
             </div>
 
             {/* Calendar Scroll Area */}
-            <div className="flex-1 overflow-y-auto p-4 sm:p-8">
-              <div className="max-w-5xl mx-auto">
+            <div className="flex-1 overflow-y-auto">
+              <div className="max-w-5xl mx-auto p-4 sm:p-8">
                 {renderCalendar()}
               </div>
             </div>
